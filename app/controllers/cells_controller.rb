b03471b24c03cf518e16a8395a33c5a6a15c3fe5
Marat Nasyrov <marat.online@live.ru>
2016-03-28 02:27:44 +0300

    write live_neighbors method

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -1,11 +1,12 @@
 require '..app/model/cell'
 
 class CellsController
-  attr_accessor :cells, :torus_surface, :live_cells_number
+  attr_accessor :cells, :torus_surface, :live_cells_number, :neighbors_number
 
   def initialize(cells)
     @cells = cells
     @live_cells_number = 0
+    @neighbors_number = 0
   end
 
   def get_cells
@@ -41,5 +42,17 @@ class CellsController
   end
 
   def live_neighbors(i,j)
+    neighbors_number = 0
+
+    [i-1, i, i+1].each do |ind1|
+      x = ind1 % cells.size
+      [j-1,j, j+1].each do |ind2|
+        y = ind2 % cells.size
+        if cells[x][y] == 1 && [x,y] != [i,j]
+          neighbors_number += 1
+        end
+      end
+    end
+    return neighbors_number
   end
 end
