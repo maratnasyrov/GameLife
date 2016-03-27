df5bc3835853d1e82874189245fb57868b5444ca
Marat Nasyrov <marat.online@live.ru>
2016-03-28 02:38:01 +0300

    starting_life method, end game if number of the live cells 0

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -41,5 +41,48 @@ class GameController
     return torus_surface
   end
 
+  def starting_life(cells)
+    cells = CellsController.new(cells)
+    live_cells = cells.find_live_cells
+    cells_replay = Array.new
+    current_configuration = Array.new
+    last_configuration = Array.new
+
+    while live_cells > 0 && do
+      sleep 2
+      system('clear')
+
+      evolution = Array.new(5).map!{Array.new(5)}
+      cells_array = cells.get_cells
+
+      cells_array.each_index do |i|
+        cells_array[i].each_index do |j|
+          number = cells.live_neighbors(i,j)
+          if number == 3
+            evolution[i][j] = 1
+          elsif number < 2 || number > 3
+            evolution[i][j] = 0
+          else
+            evolution[i][j] = cells_array[i][j]
+          end
+        end
+      end
+
+      last = cells.configuration(last_configuration).clone
+      cells_replay.push(last)
+      cells_array = evolution
+      @evolution = cells_array
+      cells = CellsController.new(evolution)
+
+      GameView.display_number_iteration(cells_replay.size)
+      GameView.display_number_live_cells(live_cells)
+      GameView.display_array(cells_array)
+
+      live_cells = cells.find_live_cells
+    end
+
+    GameView.display_message_end_game(live_cells, cells_replay.size)
+  end
+
   GameController.new
 end
