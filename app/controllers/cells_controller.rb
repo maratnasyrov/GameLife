cf289c7143731e4561f1269e4650f929769e3553
Marat Nasyrov <marat.online@live.ru>
2016-03-28 02:24:53 +0300

    write find_live_cells method

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -1,9 +1,11 @@
-require '../app/model/cell'
+require '..app/model/cell'
 
 class CellsController
+  attr_accessor :cells, :torus_surface, :live_cells_number
 
   def initialize(cells)
     @cells = cells
+    @live_cells_number = 0
   end
 
   def get_cells
@@ -11,6 +13,17 @@ class CellsController
   end
 
   def find_live_cells
+    live_cells_number = 0
+
+    cells.each_index do |i|
+      cells[i].each_index do |j|
+        cell = cells[i][j]
+        if cell == 1
+          live_cells_number += 1
+        end
+      end
+    end
+    return live_cells_number
   end
 
   def configuration(object)
