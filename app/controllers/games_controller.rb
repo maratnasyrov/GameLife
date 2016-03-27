d0d585a9f654b07286a93e7dd1838d4cee76ce41
Marat Nasyrov <marat.online@live.ru>
2016-03-28 02:36:31 +0300

    start_game method, create world method for 5x5 matrix

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 require '..app/controller/cells_controller'
 require '..app/view/game_view'
 
-class GamesController
+class GameController
   def initialize
     start_game
   end
@@ -21,7 +21,25 @@ class GamesController
   end
 
   def start_game
+    GameView.display_message_start_game
+
+    world = create_world(open_file)
+    starting_life(world)
   end
 
-  GamesController.new
+  def create_world(open_file)
+    torus_surface = Array.new(5).map!{Array.new(5)}
+    ind_cells = 0
+    torus_surface.each_index do |i|
+      torus_surface[i].each_index do |j|
+        cell = Cell.new.set_status(open_file[ind_cells].to_s.delete('[""]').to_i)
+        torus_surface[i][j] = cell
+        ind_cells += 1
+      end
+    end
+    GameView.display_array(torus_surface)
+    return torus_surface
+  end
+
+  GameController.new
 end
